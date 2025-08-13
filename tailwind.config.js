2578d9384b29e93b81c312e9407fe8a345f3f093
cromx123 <73903821+cromx123@users.noreply.github.com>
2025-08-13 03:33:01 -0400

    compila el sistema con tailwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,10 @@
-/** @type {import('tailwindcss').Config} */
+// tailwind.config.js
 module.exports = {
-    content: ["./src/**/*.{js,jsx,ts,tsx}"],
-    theme: { extend: {}},
-    plugins: [],
-  };
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
